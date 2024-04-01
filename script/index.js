cb2b89824f4c95786af73b5666b2f3d21a22022f
Oshah56 <aishah.teima@gmail.com>
2024-04-01 11:31:16 -0400

    index changes

--- script/index.js
+++ script/index.js
@@ -30,24 +30,18 @@ document.addEventListener('DOMContentLoaded', () => {
   
   var mobileNav = document.getElementById('mobileNav')
 
-function toggleMenu() {
-    mobileNav.classList.toggle('hidden')
-}
-
-var mobileNav = document.getElementById('mobileNav')
-
-function toggleMenu() {
-    mobileNav.classList.toggle('hidden')
-}
-
-// Get all elements with the animate-once class
-const elements = document.querySelectorAll('.animate-once');
-
-// Loop through each element
-elements.forEach(element => {
-    // Add event listener for animationend event
-    element.addEventListener('animationend', () => {
-        // Pause the animation after it completes
-        element.style.animationPlayState = 'paused';
-    }, { once: true }); // Listen for the event only once
-});
+  function toggleMenu() {
+      mobileNav.classList.toggle('hidden')
+  }
+  
+  // Get all elements with the animate-once class
+  const elements = document.querySelectorAll('.animate-once');
+  
+  // Loop through each element
+  elements.forEach(element => {
+      // Add event listener for animationend event
+      element.addEventListener('animationend', () => {
+          // Pause the animation after it completes
+          element.style.animationPlayState = 'paused';
+      }, { once: true }); // Listen for the event only once
+  });
